2958a78da3bc7f6296bc86f8161af246697c4136
Jackson <jacksonhenryckdev@gmail.com>
2025-10-06 12:58:09 -0300

    novo commit

--- Time.java
+++ Time.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Time {
+
+    String nome;
+    String dono;
+    int anoFundado;
+
+    Time (String nome, String dono, int anoFundado) {
+        this.nome = nome;
+        this.dono = dono;
+        this.anoFundado = anoFundado;
+    }
+
+    String mostrarNome(){
+        return nome;
+    }
+
+    void mostrarAno(){
+        if(anoFundado > 0 && anoFundado < 2025) {System.out.println("O ano fundado é" + anoFundado);}
+
+        else { System.out.println("Invalido.");  }
+    }
+
+
 }
+
